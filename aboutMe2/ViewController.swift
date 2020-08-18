ce5cf1aa5749d4da617604ae5fda8143ab277cc0
codegangsk <kjhj0604@gmail.com>
2020-08-18 19:30:32 +0900

    Initial Commit

---
+++ aboutMe2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aboutMe2
+//
+//  Created by Sophie Kim on 2020/08/18.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
